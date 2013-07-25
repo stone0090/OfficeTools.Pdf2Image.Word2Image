cbd7a1373c4c6732c9238175bce044b3447733c4
stone0090 <stone0090@gmail.com>
2013-07-25 19:23:22 +0800

    add 12 dll add 11 project

---
+++ pdf2image.ABCpdf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using WebSupergoo.ABCpdf6;
+
+namespace pdf2image.ABCpdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theDoc = new Doc();
+
+            theDoc.Read("C:\\FileServer\\test.pdf");
+
+            theDoc.Rendering.DotsPerInch = 200;
+            
+            for (int i = 0; i < 4; i++)
+            {
+                theDoc.PageNumber = i;
+                theDoc.Rect.String = theDoc.CropBox.String;
+                theDoc.Rendering.Save("C:\\FileServer\\" + i.ToString(CultureInfo.InvariantCulture) + ".JPG");
+            }
+        }
+    }
+}
